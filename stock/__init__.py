f6ee2bc059d13a19714fa084350a6cc493077b3f
Hamza Osman <170122078@aston.ac.uk>
2022-02-21 20:16:38 +0000

    authentication system implemented

--- stock/__init__.py
+++ stock/__init__.py
@@ -1,13 +1,55 @@
 from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from tempfile import mkdtemp
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hsjsuush sgsjs'
 
+    app.config['STATIC_FOLDER'] = 'static'
+    app.config['TEMPLATES_FOLDER'] = 'template'
+
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    app.config['SESSION_FILE_DIR'] = mkdtemp()
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+     # Ensure responses aren't cached
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+    # Initialize session
+    Session(app)
+
+    from .models import Users, Transactions
+    
+    
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    create_database(app)
+
     return app
+
+def create_database(app):
+	if not path.exists('stock/' + DB_NAME):
+		db.create_all(app=app)
+		print('Creates Database!')
--- stock/auth.py
+++ stock/auth.py
@@ -1,15 +1,83 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template , request, flash, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+from .models import Users
+from . import db
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return"<p>Login1</p"
+    """Log user in"""
+    
+    # Forget any user_id
+    session.clear()
+    
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        current_user = Users.query.filter_by(username=username).first()
+        if current_user:
+            if check_password_hash(current_user.hash, password):
+                flash('Logged in successfully!', category='success')
+                session["user_id"] = current_user.id
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Username does not exist.', category='error')
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return"<p>Logout</p"
+    session.clear()
+    flash('You were logged out', category='success')
+    return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return"<p>Sign Up</p"
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            flash('username already exists.', category='error')
+        
+        elif len(username) < 4:
+            flash('username must be greater than 3 characters.', category='error')
+
+        elif password != confirmation:
+            flash('Passwords don\'t match.', category='error')
+
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        
+        else:
+            # Insert username in database
+            new_user = Users(
+                username = username,
+                hash = generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.flush()
+                
+            # log newly registered user in
+            #id = db.session.query(Users.id).filter(Users.username == username).first()
+            session["user_id"] = new_user.id
+            
+            # finalize changes to database
+            db.session.commit()
+            
+            # Flash success message
+            flash("Congratulation! You were successfully registered!", category='success')
+            
+            # Redirect user to home page
+            
+            return redirect(url_for('views.home'))
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    return render_template("sign_up.html")
